ea60dae1ce52b4b3a01000f5c0fdf4c7b22e969e
Loïc STEINMETZ <loicsteinmetz22@gmail.com>
2020-11-27 20:07:06 +0100

    [0] Refac MigrationsLauncher

--- src/main/java/app/database/MigrationsLauncher.java
+++ src/main/java/app/database/MigrationsLauncher.java
@@ -5,10 +5,8 @@ import org.flywaydb.core.Flyway;
 import utils.EnvironmentVariablesUtils;
 import utils.LoggerUtils;
 
-import java.util.Scanner;
-
 /**
- * Lanceur des migrations via {@link Flyway}.
+ * Lanceur des processus migrations via {@link Flyway}.
  */
 public class MigrationsLauncher {
 
@@ -23,37 +21,31 @@ public class MigrationsLauncher {
       final String user = EnvironmentVariablesUtils.getString(EnvironmentVariablesUtils.DB_USER);
       final String password = EnvironmentVariablesUtils.getString(EnvironmentVariablesUtils.PASSWORD);
       Flyway flyway = Flyway.configure().dataSource(url, user, password).load();
-      execute(flyway);
-      LOGGER.info("Processus terminée.");
+      execute(flyway, args);
     } catch (Exception e) {
       e.printStackTrace();
-      LOGGER.error("Migration impossible.");
+    } finally {
+      LOGGER.info("Processus terminée.");
     }
   }
 
-  private static void execute(Flyway flyway) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("CMD > ");
-    String cmd = sc.nextLine();
-    switch (cmd.toLowerCase().trim()) {
-      case "migrate":
-        LOGGER.info("RUN : Migrate.");
-        flyway.migrate();
-        break;
-      case "repair":
-        LOGGER.info("RUN : Repair.");
-        flyway.repair();
-        break;
-      case "clean":
-        System.out.print("Confirm (yes/no) > ");
-        if (sc.nextLine().toLowerCase().trim().equals("yes")) {
-          LOGGER.info("RUN : Clean.");
+  private static void execute(Flyway flyway, String[] args) {
+    if (args.length < 1) {
+      LOGGER.error("Paramètre manquant.");
+    } else {
+      switch (args[0]) {
+        case "-migrate":
+          LOGGER.info("RUN Migrate.");
+          flyway.migrate();
+          break;
+        case "-clean":
+          LOGGER.info("RUN Clean.");
           flyway.clean();
-        }
-        break;
-      default:
-        LOGGER.error("Commande inconnue.");
-        break;
+          break;
+        default:
+          LOGGER.error("Paramètre inconnu.");
+          break;
+      }
     }
   }
 }
