deab25c8c65a96d1b60e2c58c8cb01206d9565a3
Loïc STEINMETZ <loicsteinmetz22@gmail.com>
2020-11-28 16:47:46 +0100

    [0] Séparation des logs app et logs externes

--- src/main/java/config/LoggerConfiguration.java
+++ src/main/java/config/LoggerConfiguration.java
@@ -24,36 +24,51 @@ import java.net.URI;
 public class LoggerConfiguration extends ConfigurationFactory {
 
   private static final String DEFAULT_LEVEL = "WARN";
-  private static final String DEFAULT_PATTERN = "%d{dd-MM-yyyy - HH:mm:ss} [%t] %level | %logger{36} | %msg%n";
-  private static final String APPENDER = "stdout";
+  private static final String APP_APPENDER = "app";
+  private static final String EXT_APPENDER = "ext";
+  private static final String APP_PATTERN = "%d{dd-MM-yyyy - HH:mm:ss} [%t] %level | %logger{36} | %msg%n";
+  public static final String EXT_PATTERN = "%10level | %logger{36} - %msg%n";
 
   private static Configuration createConfiguration(final String name, ConfigurationBuilder<BuiltConfiguration> builder) {
     final String level = EnvironmentVariablesUtils
       .getString(EnvironmentVariablesUtils.LOG_LEVEL, DEFAULT_LEVEL).toUpperCase();
     final String pattern = EnvironmentVariablesUtils
-      .getString(EnvironmentVariablesUtils.LOG_FORMAT, DEFAULT_PATTERN);
+      .getString(EnvironmentVariablesUtils.LOG_FORMAT, APP_PATTERN);
     builder.setConfigurationName(name);
     builder.setStatusLevel(Level.ERROR);
-    AppenderComponentBuilder console = builder.newAppender(APPENDER, "console").
-      addAttribute("target", ConsoleAppender.Target.SYSTEM_OUT);
-    console.add(builder.newLayout("PatternLayout")
-      .addAttribute("pattern", pattern));
-    setLoggers(builder, level, console);
+    setAppLogs(builder, level, pattern);
+    setExternalLogs(builder, level);
     return builder.build();
   }
 
-  private static void setLoggers(ConfigurationBuilder<BuiltConfiguration> builder, String level, AppenderComponentBuilder console) {
+  private static void setAppLogs(ConfigurationBuilder<BuiltConfiguration> builder, String level, String pattern) {
+    AppenderComponentBuilder console = builder.newAppender(APP_APPENDER, "console").
+      addAttribute("target", ConsoleAppender.Target.SYSTEM_OUT);
+    console.add(builder.newLayout("PatternLayout")
+      .addAttribute("pattern", pattern));
     builder.add(console)
       .add(builder.newRootLogger(Level.getLevel(level))
-        .add(builder.newAppenderRef(APPENDER)))
+        .add(builder.newAppenderRef(APP_APPENDER)));
+  }
+
+  private static void setExternalLogs(ConfigurationBuilder<BuiltConfiguration> builder, String level) {
+    AppenderComponentBuilder console = builder.newAppender(EXT_APPENDER, "console").
+      addAttribute("target", ConsoleAppender.Target.SYSTEM_OUT);
+    console.add(builder.newLayout("PatternLayout")
+      .addAttribute("pattern", EXT_PATTERN));
+    setExternalLoggers(builder, level, console);
+  }
+
+  private static void setExternalLoggers(ConfigurationBuilder<BuiltConfiguration> builder, String level, AppenderComponentBuilder console) {
+    builder.add(console)
       .add(builder.newLogger("org.hibernate", Level.OFF)
-        .add(builder.newAppenderRef(APPENDER))
+        .add(builder.newAppenderRef(EXT_APPENDER))
         .addAttribute("additivity", false))
       .add(builder.newLogger("org.hibernate.SQL", Level.getLevel(level))
-        .add(builder.newAppenderRef(APPENDER))
+        .add(builder.newAppenderRef(EXT_APPENDER))
         .addAttribute("additivity", false))
       .add(builder.newLogger("org.hibernate.type.descriptor.sql", Level.getLevel(level))
-        .add(builder.newAppenderRef(APPENDER))
+        .add(builder.newAppenderRef(EXT_APPENDER))
         .addAttribute("additivity", false));
   }
 
