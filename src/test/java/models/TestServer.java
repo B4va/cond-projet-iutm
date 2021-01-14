35188e482e85c2c4a8ea6bedd75c7908a6d12f94
alexisbe-github <66599552+alexisbe-github@users.noreply.github.com>
2021-01-14 15:14:22 +0100

    [1] TESTS - Tests du modèle Server
    * TestServer
    
    * Update TestServer.java
    
    * refac
    
    * Update TestServer.java
    
    * Revert "Update TestServer.java"
    
    This reverts commit 4f0dee7b4119c79c739c528517ae26697d3d27e4.
    
    * fix
    
    Co-authored-by: Alexis <metin547100@gmail.com>
    Co-authored-by: Loïc STEINMETZ <loicsteinmetz22@gmail.com>

---
+++ src/test/java/models/TestServer.java
@@ -0,0 +1,120 @@
+package models;
+
+import org.junit.jupiter.api.*;
+
+import javax.persistence.PersistenceException;
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Classe de test de {@link Server}.
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestServer implements TestModel {
+
+  private static int ID_SERVER;
+  private static int ID_SCHEDULE;
+  private static Server SERVER;
+  private static Schedule SCHEDULE;
+  private static final String PROMOTION_TEST = "test";
+  private static final String REFERENCE_TEST = "ref";
+  private static final String UPDATED_REFERENCE = "updated_ref";
+
+  @AfterAll
+  public static void tearDown() {
+    SERVER = Model.read(ID_SERVER, Server.class);
+    if (nonNull(SERVER)) {
+      SERVER.delete();
+    }
+    SCHEDULE = Model.read(ID_SCHEDULE, Schedule.class);
+    if (nonNull(SCHEDULE)) {
+      SCHEDULE.delete();
+    }
+  }
+
+  @Test
+  @Order(1)
+  @Override
+  public void testCreate() {
+    SCHEDULE = new Schedule(PROMOTION_TEST);
+    ID_SCHEDULE = SCHEDULE.create();
+    SERVER = new Server(REFERENCE_TEST, Schedule.read(ID_SCHEDULE, Schedule.class));
+    ID_SERVER = SERVER.create();
+    List<Server> servers = Model.readAll(Server.class);
+    assertTrue(servers.stream().anyMatch(s -> s.getId() == ID_SERVER));
+  }
+
+  @Test
+  @Order(2)
+  public void testCreate_reference_null() {
+    Server server = new Server();
+    server.setSchedule(Schedule.read(ID_SCHEDULE, Schedule.class));
+    assertThrows(PersistenceException.class, server::create);
+  }
+
+  @Test
+  @Order(3)
+  public void testCreate_schedule_null() {
+    Server server = new Server();
+    server.setReference(REFERENCE_TEST);
+    assertThrows(PersistenceException.class, server::create);
+  }
+
+  @Test
+  @Order(4)
+  @Override
+  public void testRead() {
+    Server s = Model.read(ID_SERVER, Server.class);
+    assertNotNull(s);
+    assertAll(
+      () -> assertNotNull(s),
+      () -> assertEquals(s.getId(), SERVER.getId()),
+      () -> assertEquals(s.getReference(), SERVER.getReference()),
+      () -> assertEquals(s.getSchedule().getId(), ID_SCHEDULE)
+    );
+  }
+
+  @Test
+  @Order(5)
+  @Override
+  public void testUpdate() {
+    SERVER.setReference(UPDATED_REFERENCE);
+    SERVER.setSchedule(SCHEDULE);
+    SERVER.update();
+    SERVER = Model.read(ID_SERVER, Server.class);
+    assertNotNull(SERVER);
+    assertEquals(SERVER.getReference(), UPDATED_REFERENCE);
+  }
+
+  @Test
+  @Order(6)
+  public void testUpdate_reference_null() {
+    SERVER.setReference(null);
+    assertThrows(PersistenceException.class, SERVER::update);
+  }
+
+  @Test
+  @Order(7)
+  public void testUpdate_schedule_null() {
+    SERVER.setSchedule(null);
+    assertThrows(PersistenceException.class, SERVER::update);
+  }
+
+  @Test
+  @Order(8)
+  public void testDelete_schedule_with_associated_server() {
+    assertThrows(PersistenceException.class, SCHEDULE::delete);
+  }
+
+  @Test
+  @Order(9)
+  @Override
+  public void testDelete() {
+    Model.read(ID_SERVER, Server.class).delete();
+    Server s = Model.read(ID_SERVER, Server.class);
+    assertNull(s);
+  }
+
+}
