9e28dd09f283b18f156cd6b63db6793138c3e131
alexisbe-github <66599552+alexisbe-github@users.noreply.github.com>
2021-02-28 14:49:21 +0100

    [DEV] Process de création d'une Task

---
+++ src/main/java/exceptions/InvalidDataException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+/**
+ * Exception retournée lorsqu'une tâche est invalide
+ * {@link models.Task} à une fonctionnalité.
+ */
+public class InvalidDataException extends Exception{
+}
